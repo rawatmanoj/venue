3da807e77fb6c1c82d54baa6734159add1499269
rawatmanoj <42403468+rawatmanoj@users.noreply.github.com>
2020-02-13 02:04:41 +0530

    adding

--- src/components/Highlights/Discount.js
+++ src/components/Highlights/Discount.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
 import Slide from 'react-reveal/Slide';
 import Slider from 'react-slick';
+import MyButton from '../utils/MyButton'
 
 class Discount extends Component {
     
@@ -43,7 +44,13 @@ class Discount extends Component {
                         <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
                          
                          <div>
-                           button
+                        < MyButton
+                           text="Tickets"
+                           bck="#ffa800"
+                           color="#ffffff"
+                           link="#"
+                        
+                        />
                          </div>
  
 
