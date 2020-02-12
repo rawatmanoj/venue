5569c4cefd512534e6399c16e036b606c3f5b1f0
rawatmanoj <42403468+rawatmanoj@users.noreply.github.com>
2020-02-13 01:39:16 +0530

    adding

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './resources/styles.css';
 import Header from './components/header_footer/header'
 import Featured from './components/featured'
 import VenueInfo from './components/venueinfo/VenueInfo'
+import HighLights from './components/Highlights'
 
 class App extends React.Component {
     render(){
         return(
-            <div className="App" style={{height:"1500px",backgroundColor:"lightBlue"}}>
+            <div className="App" style={{height:"1500px"}}>
                 <Header/>
                 <Featured/>
                 <VenueInfo/>
+                <HighLights/>
             </div>
         );
     }
---
+++ src/components/Highlights/Discount.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import Fade from 'react-reveal/Fade';
+import Slide from 'react-reveal/Slide';
+import Slider from 'react-slick';
+
+class Discount extends Component {
+    
+    state={
+       discountStart:0,
+       discountEnd:30
+    };
+
+    percentage=()=>{
+         if(this.state.discountStart<this.state.discountEnd){
+             this.setState({
+                 discountStart:this.state.discountStart+1
+             });
+         }
+    }
+
+    componentDidUpdate(){
+        setTimeout(()=>{
+           this.percentage();
+        },30);
+    }
+
+    render() {
+        return (
+            <div className="center_wrapper">
+                <div className="discount_wrapper">
+                    <Fade
+                      onReveal={()=>this.percentage()}
+                    >
+                    <div className="discount_porcentage">
+                        <span>{this.state.discountStart}%</span>
+                        <span>OFF</span>
+                    </div>
+                    </Fade>
+
+                    <Slide right>
+                    <div className="discount_description">
+                        <h3>Purchase ticket before 20th Dec</h3> 
+                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                         
+                         <div>
+                           button
+                         </div>
+ 
+
+                    </div>
+                    </Slide>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Discount;
